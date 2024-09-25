1d40c1933b045a3fb43125e00188c4cf392f436f
raghu28 <raghavavmurthy@gmail.com>
2024-09-25 20:06:44 +0530

    raghu

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
-    timeout:20000,
-    headers: {"Content-Type":"application/json"}
+    baseURL: 'https://backend12345.netlify.app', // Change this to your deployed URL
+    timeout: 50000,
+    headers: { "Content-Type": "application/json" }
 });
